e142a5a70ff4255e2a438a84342ec0f925ea32e2
Chinbat Orchlon <chinbatorchlon@Chinbats-MacBook-Pro.local>
2023-01-11 21:13:21 -0800

    added json data

--- pages/events/[cat]/index.tsx
+++ pages/events/[cat]/index.tsx
@@ -2,7 +2,6 @@ const Page = () => {
   return (
     <div>
       <h1>Events in London</h1>
-
       <a href="/events/event1">Event 1</a>
       <a href="/events/event2">Event 2</a>
       <a href="/events/event3">Event 3</a>
