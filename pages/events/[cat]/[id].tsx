b0368b9adbdf9f4a9d59d2f4690fb8f9266f8bcf
orchlonn <orchllnz@gmail.com>
2023-01-16 14:53:59 -0800

    fetched all the datum

--- pages/events/[cat]/[id].tsx
+++ pages/events/[cat]/[id].tsx
@@ -1,5 +1,45 @@
-const EventPage = () => {
-  return <h1>Our single event</h1>;
+import Image from "next/image";
+
+const EventPage = (props: any) => {
+  return (
+    <div>
+      <Image
+        src={props.data.image}
+        width={1000}
+        height={600}
+        alt={props.data.title}
+      />
+      <h1>{props.data.title}</h1>
+      <p>{props.data.description}</p>
+    </div>
+  );
 };
 
 export default EventPage;
+
+export async function getStaticPaths() {
+  const data = await import("../../../data/data.json");
+  const allEvents = data.allEvents;
+
+  const allPaths = allEvents.map((path) => {
+    return {
+      params: { id: path.id, cat: path.city },
+    };
+  });
+
+  return {
+    paths: allPaths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context: any) {
+  const { allEvents } = await import("../../../data/data.json");
+
+  const id = context.params.id;
+  const eventData = allEvents.find((ev) => ev.id === id);
+
+  return {
+    props: { data: eventData },
+  };
+}
